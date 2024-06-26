c5ec9a02225d97bec0f71e1abdac47f96f347b43
Mukul Kumar <mkunboxing@gmail.com>
2024-06-26 23:48:55 +0530

    header fixed for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, Button, IconButton, Drawer, List, ListItem, ListItemText } from '@mui/material';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleTheme } from '../redux/blogSlice';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
+import MenuIcon from '@mui/icons-material/Menu';
 import { logout } from '../redux/authSlice'; // Assuming logout action from authSlice
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 
 function Header() {
   const dispatch = useDispatch();
@@ -13,6 +16,9 @@ function Header() {
   const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
   const navigate = useNavigate();
   const location = useLocation();
+  const themeObj = useTheme();
+  const isMobile = useMediaQuery(themeObj.breakpoints.down('sm'));
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   const handleCreatePostClick = () => {
     if (isLoggedIn) {
@@ -33,26 +39,70 @@ function Header() {
     textDecoration: location.pathname === path ? 'underline' : 'none',
     fontWeight: location.pathname === path ? 'bold' : 'normal',
   });
+
+  const menuItems = (
+    <>
+      <Button color="inherit" component={Link} to="/" style={linkStyle('/')}>Home</Button>
+      <Button color="inherit" component={Link} to="/category/technology" style={linkStyle('/category/technology')}>Technology</Button>
+      <Button color="inherit" component={Link} to="/category/travel" style={linkStyle('/category/travel')}>Travel</Button>
+      <Button color="inherit" component={Link} to="/category/food" style={linkStyle('/category/food')}>Food</Button>
+      <Button color="inherit" component={Link} to="/category/lifestyle" style={linkStyle('/category/lifestyle')}>Lifestyle</Button>
+      
+      {isLoggedIn ? (
+        <>
+          <Button color="inherit" onClick={handleCreatePostClick}>Create Post</Button>
+          <Button color="inherit" onClick={handleLogoutClick}>Logout</Button>
+        </>
+      ) : (
+        <Button component={Link} to="/login" color="inherit" style={linkStyle('/login')}>Login</Button>
+      )}
+    </>
+  );
+
   return (
     <AppBar position="sticky">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <Typography>Blog App</Typography>
+          <Link to="/" style={{ color: 'inherit', textDecoration: 'none' }}>Blog App</Link>
         </Typography>
-        <Button color="inherit" component={Link} to="/" style={linkStyle('/')}>Home</Button>
-        <Button color="inherit" component={Link} to="/category/technology" style={linkStyle('/category/technology')}>Technology</Button>
-        <Button color="inherit" component={Link} to="/category/travel" style={linkStyle('/category/travel')}>Travel</Button>
-        <Button color="inherit" component={Link} to="/category/food" style={linkStyle('/category/food')}>Food</Button>
-        <Button color="inherit" component={Link} to="/category/lifestyle" style={linkStyle('/category/lifestyle')}>Lifestyle</Button>
-        
-        {isLoggedIn ? (
+        {isMobile ? (
           <>
-          <Button color="inherit" onClick={handleCreatePostClick}>Create Post</Button>
-          <Button color="inherit" onClick={handleLogoutClick}>Logout</Button>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={() => setDrawerOpen(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Drawer
+              anchor="right"
+              open={drawerOpen}
+              onClose={() => setDrawerOpen(false)}
+            >
+              <List>
+                {menuItems.props.children.map((item, index) => (
+                  <ListItem button key={index} onClick={() => setDrawerOpen(false)}>
+                    {item.type === Button ? (
+                      <Button
+                        color="inherit"
+                        component={item.props.component}
+                        to={item.props.to}
+                        style={linkStyle(item.props.to)}
+                        fullWidth
+                      >
+                        {item.props.children}
+                      </Button>
+                    ) : (
+                      <ListItemText primary={item.props.children} />
+                    )}
+                  </ListItem>
+                ))}
+              </List>
+            </Drawer>
           </>
-          
         ) : (
-          <Button component={Link} to="/login" color="inherit" style={linkStyle('/login')}>Login</Button>
+          menuItems
         )}
         <IconButton onClick={() => dispatch(toggleTheme())} color="inherit">
           {theme === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
